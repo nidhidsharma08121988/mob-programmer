5eee93ab70b9617f2655055802102b4450f4b486
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-29 18:34:48 +0400

    test pass: must display participant-list element

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='App'>
       <input data-testId='participant-input'></input>
+      <div data-testId='participant-list'></div>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const inputElement = screen.getByTestId('participant-input')
+  const inputElement = screen.queryByTestId('participant-input')
   expect(inputElement).toBeInTheDocument()
+})
+
+test('must display list of participants', () => {
+  render(<App />)
+  const listElement = screen.queryByTestId('participant-list')
+  expect(listElement).toBeTruthy()
 })
