12ead976c6e51ba3cd6111b6bcc5ba6af81963cc
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-29 18:31:07 +0400

    refactor: rename variable element in test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByTestId('participant-input')
-  expect(linkElement).toBeInTheDocument()
+  const inputElement = screen.getByTestId('participant-input')
+  expect(inputElement).toBeInTheDocument()
 })
