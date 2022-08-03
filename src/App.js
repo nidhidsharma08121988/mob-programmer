125b2eb5fb505c079c901128beecf78f39051b9b
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-08-03 16:42:03 +0400

    feat: try to implement adding participant feature on the frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [participant, setParticipant] = useState('')
+  const handleEnterKey = e => {
+    if (e.key === 'Enter') {
+      //call function join from Mob
+      //reset the input box
+    }
+  }
   return (
     <div className='App'>
-      <input data-testid='participant-input'></input>
+      <input
+        data-testid='participant-input'
+        value={participant}
+        onChange={e => setParticipant(e.target.value)}
+        onKeyDown={handleEnterKey}
+      ></input>
       <div data-testid='participant-list'></div>
     </div>
   )
