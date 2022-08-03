573ce4f1cd9f4e06437c40ab5ea294bc76b06f8e
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-08-03 17:04:29 +0400

    refactor: test to extract render(<App>) into a function

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,13 +2,19 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 test('renders learn react link', () => {
-  render(<App />)
+  renderApp()
   const inputElement = screen.queryByTestId('participant-input')
   expect(inputElement).toBeInTheDocument()
 })
 
 test('must display list of participants', () => {
-  render(<App />)
+  renderApp()
   const listElement = screen.queryByTestId('participant-list')
   expect(listElement).toBeTruthy()
 })
+
+
+function renderApp() {
+  render(<App />)
+}
+// test('when Alex joins mob they should appear as a participant', () => {})
