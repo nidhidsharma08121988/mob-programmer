7197bae52e22561f528d8f98b73c799d76a54bd3
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-08-03 17:01:58 +0400

    undo: all changes to implement them using TDD

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+const Mob = require('./Mob')
+
 function App() {
-  const [participant, setParticipant] = useState('')
-  const handleEnterKey = e => {
-    if (e.key === 'Enter') {
-      //call function join from Mob
-      //reset the input box
-    }
-  }
   return (
     <div className='App'>
-      <input
-        data-testid='participant-input'
-        value={participant}
-        onChange={e => setParticipant(e.target.value)}
-        onKeyDown={handleEnterKey}
-      ></input>
+      <input data-testid='participant-input'></input>
       <div data-testid='participant-list'></div>
     </div>
   )
