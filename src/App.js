4c1e65bf3845f777ba7fde46b18f175e6b3abe55
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-08-04 13:02:36 +0400

    implement add participant to list feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const Mob = require('./Mob')
 
 function App() {
+  const [mob, setMob] = useState(null)
+  const [participant, setParticipant] = useState('')
+  const [participants, setParticipants] = useState([])
+
+  useEffect(() => {
+    setMob(new Mob())
+  }, [])
+
+  const handleEnter = e => {
+    if (e.key === 'Enter') {
+      mob.join(participant)
+      setParticipant('')
+      const newParticipants = [...participants, participant]
+      setParticipants(newParticipants)
+    }
+  }
+
   return (
     <div className='App'>
-      <input data-testid='participant-input'></input>
-      <div data-testid='participant-list'></div>
+      <input
+        data-testid='participant-input'
+        value={participant}
+        onChange={e => setParticipant(e.target.value)}
+        onKeyDown={handleEnter}
+      ></input>
+      <div data-testid='participant-list'>
+        {participants.map((participant, index) => (
+          <li key={index}>{participant}</li>
+        ))}
+      </div>
     </div>
   )
 }
